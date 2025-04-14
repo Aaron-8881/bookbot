fc56346c5309d49baf7e878fdb52ab4586ef134d
Aaron-8881 <aaronslifeessentials@gmail.com>
2025-04-13 20:32:24 -0700

    Read function

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as file:
+        return file.read()
+    
+def main():
+    book_path = "books/frankenstein.txt"
+    book_content = get_book_text(book_path)
+    print(book_content)
+
+if __name__ == "__main__":
+    main()
